50cbd210e9c95c2f0cbb2a03e6665b383d877204
Mateus Lopes <92342656+mateusmedeir@users.noreply.github.com>
2021-11-08 20:14:16 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:11,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        420:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:4
+        },
+        1100:{
+            items:5
+        }
+    }
+})
